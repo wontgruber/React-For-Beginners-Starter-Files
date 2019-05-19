55b6601a9f71b498458ba14c38463aa16bc010d5
Nick Wilgruber <nwilgruber@gmail.com>
2019-05-19 13:39:55 -0400

    Add input Ref

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = event => {
+        event.preventDefault();
+        console.log(this);
+        console.log(this.myInput);
+        const storeName = this.myInput.value;
+        this.props.history.push(`/store/${storeName}`);
+    }
     render() {
         return (
-        <form action="" className="store-selector">
-            <input type="text" required placeholder="Store Name"></input>
+        <form className="store-selector" onSubmit={this.goToStore}>
+            <h2>Please Enter A Store</h2>
+            <input 
+                type="text" 
+                ref={this.myInput}
+                required
+                placeholder="Store Name" 
+                defaultValue={getFunName()}
+            />
             <button type="submit">Visit Store →</button>
         </form>
         )
