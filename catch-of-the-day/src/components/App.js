f8ccb805a95e95dd1a05bc60d5040fc0a7b31110
Nick Wilgruber <nwilgruber@gmail.com>
2019-05-23 17:05:13 -0400

    Use state!

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,14 +4,28 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {},
+    };
+
+    addFish = fish => {
+        // 1. Take a copy of existing state
+        const fishes = { ...this.state.fishes };
+        // 2. Add new fish
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. Set new state
+        this.setState({ fishes });
+    };
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline='Fresh Seafood Market'></Header>
                 </div>
-                <Inventory></Inventory>
                 <Order></Order>
+                <Inventory addFish={this.addFish}></Inventory>
             </div>
         )
     }
