115423232291be7bf4e654d7dd0794b5cf1e23e1
Nick Wilgruber <nwilgruber@gmail.com>
2019-05-26 14:03:12 -0400

    Update Order state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -22,6 +22,11 @@ class App extends React.Component {
     loadSampleFishes = () => {
         this.setState({ fishes: sampleFishes })
     };
+    addToOrder = (key) => {
+        const order = { ...this.state.order };
+        order[key] = order[key] + 1 || 1;
+        this.setState({ order });
+    }
 
     render() {
         return (
@@ -29,7 +34,8 @@ class App extends React.Component {
                 <div className="menu">
                     <Header tagline='Fresh Seafood Market'></Header>
                     <ul className="fishes">
-                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                        {Object.keys(this.state.fishes).map(key => 
+                            <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)}
                     </ul>
                 </div>
                 <Order></Order>
